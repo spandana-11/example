b3f0d1a83c40486f66031e2670d29ad325b28dea
spandana-11 <spandanaguntupalli@gmail.com>
2024-03-06 16:07:34 +0530

    changes in simple intrest

--- src/components/ReactProjectFolder/IntrestProject.js
+++ src/components/ReactProjectFolder/IntrestProject.js
@@ -2,30 +2,65 @@ import React from "react";
 import { useState } from "react";
 
 function IntrestProject() {
-  const[principalamount,setprincipalamount]=useState("")
-  const[annualrate,setannualrate]=useState("")
-  const[periodunit,setperiodunit]=useState("")
-  const[tacc,settacc]=useState("")
+  const [Aamount, setAamount] = useState("");
+  const [intrest, setIntrest] = useState("");
+  const [data, setData] = useState({
+    principleAmount: "",
+    rateofIntrest: "",
+    period: "",
+    periodUnit: "",
+    value: "",
+  });
+  console.log(data);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    switch (data.periodUnit) {
+      case "yearly":
+        const year = data.period * 12;
+        const intrest = data.rateofIntrest / 100;
+        const principalamount = data.principleAmount;
+        const Aamount = principalamount * (1 + (intrest / 12) * year);
+        setAamount(Aamount);
+        setIntrest(Aamount - principalamount);
 
-const handlepamt=(e)=>{
-  setprincipalamount(e.target.value);
-  console.log(principalamount)
-}
-const handleannrate=(e)=>{
-  setannualrate(e.target.value);
-}
+        break;
+      case "monthly":
+        const monthly = data.period;
+        const mintrest = data.rateofIntrest / 100;
+        const mprincipalamount = data.principleAmount;
+        const mAamount = mprincipalamount * (1 + (mintrest / 12) * monthly);
+        setAamount(mAamount);
+        setIntrest(mAamount - mprincipalamount);
 
-const handleperiodunit=(e)=>{
-  setperiodunit(e.target.value)
-  console.log(periodunit)
-}
+        break;
+        case "weekly":
+          const weekly = data.period;
+          const wintrest = data.rateofIntrest / 100;
+          const wprincipalamount = data.principleAmount;
+          const wAamount = wprincipalamount * (1 + (wintrest / 52) * weekly);
+          setAamount(wAamount);
+          setIntrest(wAamount - wprincipalamount);
+  
+          break;
+          case "day":
+          const daily = data.period;
+          const dintrest = data.rateofIntrest / 100;
+          const dprincipalamount = data.principleAmount;
+          const dAamount = dprincipalamount * (1 + (dintrest / 365) * daily);
+          setAamount(dAamount);
+          setIntrest(dAamount - dprincipalamount);
+  
+          break;
+      default:
+        break;
+    }
+  };
 
-// settacc(principalamount(1+annualrate*periodunit))
-
+  // settacc(principalamount(1+annualrate*periodunit))
 
   return (
     <div className="mainpage">
-      <form action="#" >
+      <form action="#" onSubmit={handleSubmit}>
         <div className="datafields">
           <label htmlFor="intresttype">Intrest type</label>
           <select id="intresttype">
@@ -34,46 +69,73 @@ const handleperiodunit=(e)=>{
           </select>
         </div>
         <div className="datafields">
-            <label htmlFor="principalamount">Principal Amount (Rs).optional</label>
-            <input type="text" id="principalamount" value={principalamount} onChange={handlepamt}/>
+          <label htmlFor="principalamount">
+            Principal Amount (Rs).optional
+          </label>
+          <input
+            type="text"
+            id="principalamount"
+            onChange={(e) => {
+              setData({ ...data, principleAmount: e.target.value });
+            }}
+          />
         </div>
         <div className="datafields">
-            <label htmlFor="annualrate">Annual rate Optional</label>
-            <input type="text" name="annualrate" id="annualrate" value={annualrate} onChange={handleannrate}/>
+          <label htmlFor="annualrate">Annual rate Optional</label>
+          <input
+            type="text"
+            name="annualrate"
+            id="annualrate"
+            onChange={(e) => {
+              setData({ ...data, rateofIntrest: e.target.value });
+            }}
+          />
         </div>
         <div className="datafields">
-  <label htmlFor="periodunit">Period Unit</label>
-  <select id="periodunit" value={periodunit} onChange={handleperiodunit}>
-    
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-  </select>
-</div>
+          <label htmlFor="periodunit">Period Unit</label>
+          <select
+            id="periodunit"
+            onChange={(e) => {
+              setData({ ...data, periodUnit: e.target.value });
+            }}
+          >
+            <option selected>select period</option>
+            <option value="yearly">yearly</option>
+            <option value="monthly">monthly</option>
+            <option value="weekly">weekly</option>
+            <option value="day">day</option>
+          </select>
+        </div>
 
         <div className="datafields">
-            <label htmlFor="period optional">Period Optional</label>
-            <input type="text" id="period optional"/>
+          <label htmlFor="period optional">Period Optional</label>
+          <input
+            type="text"
+            id="period optional"
+            onChange={(e) => {
+              setData({ ...data, period: e.target.value });
+            }}
+          />
         </div>
+        <button className="btn btn-primary mt-2">submit</button>
+
         <div className="datafields">
           <div className="intrestdiv amount">
-          <h6>Intrest Earned :</h6>
-           
+            <h6>Intrest Earned :</h6>
+            <span>{Math.floor(intrest)}</span>
           </div>
+
           <div className="priciplediv amount">
-          <h6>Principal Amount:</h6>
-          <span>{principalamount}</span>
+            <h6>Principal Amount:</h6>
+            <span>{Math.floor(data.principleAmount)}</span>
           </div>
           <div className="totalvalue amount">
-          <h6>Total value:</h6>
-          <span>{tacc}</span>
+            <h6>Total value:</h6>
+            <span>{Math.floor(Aamount)}</span>
           </div>
         </div>
-
       </form>
-      
     </div>
-   
   );
 }
 
